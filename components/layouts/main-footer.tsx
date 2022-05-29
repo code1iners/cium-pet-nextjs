96606afdb73e27aa10a0e37bb19b8ca3b1eb7cc8
Codeliner <codeliner96@gmail.com>
2022-05-29 18:43:41 +0900

    [v0.5.0] Implement main footer component.

--- components/layouts/main-footer.tsx
+++ components/layouts/main-footer.tsx
@@ -1,3 +1,13 @@
+import Image from "next/image";
+import LogoImage from "@/assets/logos/cat.png";
+
 export default function MainFooter() {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="flex flex-col items-center p-2 border-t">
+      <div className="flex items-center gap-2">
+        <Image src={LogoImage} width={30} height={30} />
+        <h1 className="text-gradient">cium</h1>
+      </div>
+    </footer>
+  );
 }
